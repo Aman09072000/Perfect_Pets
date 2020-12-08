9ccefe57d4a0886fa4fb3a4720cef8b3471f87ef
[Aman Gupta] <[aman09072000@gmail.com]>
2020-12-08 13:24:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/Data/PetContract.java
@@ -0,0 +1,21 @@
+package com.example.Data;
+
+import android.provider.BaseColumns;
+
+public class PetContract {
+    private PetContract() {}
+
+    public static final class PetEntry implements BaseColumns{
+        public final static String TABLE_NAME = "pets";
+
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_PET_NAME = "name";
+        public final static String COLUMN_PET_BREED = "breed";
+        public final static String COLUMN_PET_GENDER = "gender";
+        public final static String COLUMN_PET_WEIGHT = "weight";
+
+        public final static int GENDER_UNKNOWN = 0;
+        public final static int GENDER_MALE = 1;
+        public final static int GENDER_FEMALE = 2;
+    }
+}
---
+++ app/src/main/java/com/example/perfectpets/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.perfectpets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.Data.PetContract;
+import com.example.Data.PetDbHelper;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+        displayDatabaseInfo();
+    }
+
+    private void displayDatabaseInfo() {
+        PetDbHelper mDbHelper = new PetDbHelper(this);
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM "  + PetContract.PetEntry.TABLE_NAME,null);
+        try {
+            TextView displayView = (TextView)findViewById(R.id.text_view_pet);
+            displayView.setText("Numbers of Rows in pets database table: "+cursor.getCount());
+        }finally {
+            cursor.close();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_catalog.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Delete all entries" menu option
+            case R.id.action_delete_all_entries:
+                // Do nothing for now
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
